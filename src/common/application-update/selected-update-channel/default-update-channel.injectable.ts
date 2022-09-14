f0b6d46211055a6575a35957baa487db2c25d8a9
Sebastian Malton <sebastian@malton.name>
2022-09-14 15:13:56 -0400

    Fix tests by converting BuildSemanticVersion and DefaultUpdateChannel to LazyInitializableState
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/common/application-update/selected-update-channel/default-update-channel.injectable.ts
+++ src/common/application-update/selected-update-channel/default-update-channel.injectable.ts
@@ -2,11 +2,11 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
-import { createInitializableState } from "../../initializable-state/create";
 import releaseChannelInjectable from "../../vars/release-channel.injectable";
+import { createLazyInitializableState } from "../../initializable-state/create-lazy";
 import { updateChannels } from "../update-channels";
 
-const defaultUpdateChannelInjectable = createInitializableState({
+const defaultUpdateChannelInjectable = createLazyInitializableState({
   id: "default-update-channel",
   init: (di) => updateChannels[di.inject(releaseChannelInjectable).get()],
 });
